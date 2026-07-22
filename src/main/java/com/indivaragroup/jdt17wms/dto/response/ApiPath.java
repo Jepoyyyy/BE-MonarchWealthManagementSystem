a10daad42dcfd3af2092389cb1f3bcdc1e0771ac
Testboiz <rioferdian2004@gmail.com>
2026-07-22 09:50:03 +0700

    refactor : removes duplicated hardcode too long URI

--- src/main/java/com/indivaragroup/jdt17wms/dto/response/ApiPath.java
+++ src/main/java/com/indivaragroup/jdt17wms/dto/response/ApiPath.java
@@ -5,27 +5,29 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApiPath {
+  public static final String ROOT_URL = "/api/v1";
+
     //Auth
-    public static final String BASE_AUTH_PATH = "/api/v1/auth";
+    public static final String BASE_AUTH_PATH = ROOT_URL + "/auth";
     public static final String LOGIN_PATH= "/login";
     public static final String REGISTER_PATH = "/register";
     public static final String LOGOUT_PATH = "/logout";
     public static final String REFRESH_TOKEN_PATH  = "/refresh";
 
     //Admin
-    public static final String BASE_ADMIN_PATH ="/api/v1/admin";
+    public static final String BASE_ADMIN_PATH =ROOT_URL + "/admin";
 
 
-    public static final String BASE_USER_PATH = "/api/v1/me";
-    public static final String BASE_ASSETS_PATH = "/api/v1/me/assets";
-    public static final String BASE_GOALS_PATH = "/api/v1/me/goals";
+    public static final String BASE_USER_PATH = ROOT_URL + "/me";
+    public static final String BASE_ASSETS_PATH = ROOT_URL + "/me/assets";
+    public static final String BASE_GOALS_PATH = ROOT_URL + "/me/goals";
 
     //product
-    public static final String BASE_PRODUCTS_PATH = "/api/v1/products";
+    public static final String BASE_PRODUCTS_PATH = ROOT_URL + "/products";
 
     //risk profiler
-    public static final String BASE_PROFILER_PATH = "/api/v1/me/profiler";
+    public static final String BASE_PROFILER_PATH = ROOT_URL + "/me/profiler";
 
     //users
-    public static final String BASE_USERS_PATH = "/api/v1/users";
+    public static final String BASE_USERS_PATH = ROOT_URL + "/users";
 }
